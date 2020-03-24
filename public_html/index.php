17dc5b78d4b4a3d83ec92bf7053d92d92048d6f1
manuel.aquino@trocafone.com <manuel.aquino@trocafone.com>
2020-03-24 04:10:48 -0300

    add domain var for customise ip from server

--- public_html/index.php
+++ public_html/index.php
@@ -1,30 +1,30 @@
 <h1>SAR is alive!</h1>
 <?php
 include('connectDb.php');
-
+$domain = "";
 ?>
-<a href="http://localhost:9999/grabaDatos.php?arduino_id=1&client_id=1&humidity=10&light=20&temperature=30">http://localhost:9999/grabaDatos.php?arduino_id=1&client_id=1&humidity=10&light=20&temperature=30</a>
+<a href="<?=$domain?>/grabaDatos.php?arduino_id=1&client_id=1&humidity=10&light=20&temperature=30" target="_blank"><?=$domain?>/grabaDatos.php?arduino_id=1&client_id=1&humidity=10&light=20&temperature=30</a>
 <br>
-<a href="http://localhost:9999/getAllowedSensorsValue.php?arduino_id=1">http://localhost:9999/getAllowedSensorsValue.php?arduino_id=1</a>
+<a href="<?=$domain?>/getAllowedSensorsValue.php?arduino_id=1" target="_blank"><?=$domain?>/getAllowedSensorsValue.php?arduino_id=1</a>
 <br>
-<a href="http://localhost:9999/getIrrigationsPlans.php?enabled=1">http://localhost:9999/getIrrigationsPlans.php?enabled=1</a>
+<a href="<?=$domain?>/getIrrigationsPlans.php?enabled=1" target="_blank"><?=$domain?>/getIrrigationsPlans.php?enabled=1</a>
 <br>
-<a href="http://localhost:9999/getUsers.php">http://localhost:9999/getUsers.php</a>
+<a href="<?=$domain?>/getUsers.php" target="_blank"><?=$domain?>/getUsers.php</a>
 <br>
-<a href="http://localhost:9999/readArduinoMeasurements.php?arduino_id=1">http://localhost:9999/readArduinoMeasurements.php?arduino_id=1</a>
+<a href="<?=$domain?>/readArduinoMeasurements.php?arduino_id=1" target="_blank"><?=$domain?>/readArduinoMeasurements.php?arduino_id=1</a>
 <br>
-<a href="http://localhost:9999/changeIrrigationPlan.php?arduino_id=1&irrigation_plan_id=2&user_id=3">http://localhost:9999/changeIrrigationPlan.php?arduino_id=1&irrigation_plan_id=2&user_id=3</a>
+<a href="<?=$domain?>/changeIrrigationPlan.php?arduino_id=1&irrigation_plan_id=2&user_id=3" target="_blank"><?=$domain?>/changeIrrigationPlan.php?arduino_id=1&irrigation_plan_id=2&user_id=3</a>
 <br>
-<a href="http://localhost:9999/getCurrentIrrigationPlanSelected.php?arduino_id=1&user_id=1">http://localhost:9999/getCurrentIrrigationPlanSelected.php?arduino_id=1&user_id=1</a>
-<br>
-
-<a href="http://localhost:9999/createUser.php?client_id=1&name='nombre'&lastname='apellido'&mail='algo@algo.com.ar'&password='name'">http://localhost:9999/createUser.php?client_id=1&name='nombre'&lastname='apellido'&mail='algo@algo.com.ar'&password='name'</a>
-<br>
-<a href="http://localhost:9999/updateUser.php?client_id=4&user_id=4&name='nombre'&lastname='apellido'&mail='algo@algo.com.ar'&password='nombre'&enabled=1">http://localhost:9999/updateUser.php?client_id=4&user_id=1&name='nombre'&lastname='apellido'&mail='algo@algo.com.ar'&password='nombre'&enabled=1</a>
+<a href="<?=$domain?>/getCurrentIrrigationPlanSelected.php?arduino_id=1&user_id=1" target="_blank"><?=$domain?>/getCurrentIrrigationPlanSelected.php?arduino_id=1&user_id=1</a>
 <br>
 
-<a href="http://localhost:9999/createIrrigationPlan.php?name='nuevo'&irrigation_plan_id=1&humidity_min_allowed=10&light_max_allowed=20&temperature_max_allowed=40&enabled=1">http://localhost:9999/createIrrigationPlan.php?name='nuevo'&irrigation_plan_id=1&humidity_min_allowed=10&light_max_allowed=20&temperature_max_allowed=40&enabled=1</a>
+<a href="<?=$domain?>/createUser.php?client_id=1&name='nombre'&lastname='apellido'&mail='algo@algo.com.ar'&password='name'" target="_blank"><?=$domain?>/createUser.php?client_id=1&name='nombre'&lastname='apellido'&mail='algo@algo.com.ar'&password='name'</a>
 <br>
-<a href="http://localhost:9999/updateIrrigationPlan.php?irrigation_plan_id=11&name=%27Plan%20b%20para%20arroz%27&humidity_min_allowed=10&light_max_allowed=20&temperature_max_allowed=30&enabled=1">http://localhost:9999/updateIrrigationPlan.php?irrigation_plan_id=11&name=%27Plan%20b%20para%20arroz%27&humidity_min_allowed=10&light_max_allowed=20&temperature_max_allowed=30&enabled=1</a>
+<a href="<?=$domain?>/updateUser.php?client_id=4&user_id=4&name='nombre'&lastname='apellido'&mail='algo@algo.com.ar'&password='nombre'&enabled=1" target="_blank"><?=$domain?>/updateUser.php?client_id=4&user_id=1&name='nombre'&lastname='apellido'&mail='algo@algo.com.ar'&password='nombre'&enabled=1</a>
+<br>
+
+<a href="<?=$domain?>/createIrrigationPlan.php?name='nuevo'&irrigation_plan_id=1&humidity_min_allowed=10&light_max_allowed=20&temperature_max_allowed=40&enabled=1" target="_blank"><?=$domain?>/createIrrigationPlan.php?name='nuevo'&irrigation_plan_id=1&humidity_min_allowed=10&light_max_allowed=20&temperature_max_allowed=40&enabled=1</a>
+<br>
+<a href="<?=$domain?>/updateIrrigationPlan.php?irrigation_plan_id=11&name=%27Plan%20b%20para%20arroz%27&humidity_min_allowed=10&light_max_allowed=20&temperature_max_allowed=30&enabled=1" target="_blank"><?=$domain?>/updateIrrigationPlan.php?irrigation_plan_id=11&name=%27Plan%20b%20para%20arroz%27&humidity_min_allowed=10&light_max_allowed=20&temperature_max_allowed=30&enabled=1</a>
 <br>
 
