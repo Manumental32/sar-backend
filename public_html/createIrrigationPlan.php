c1ba0f951240e16184d5721a662ce6bd479c36a1
manuel.aquino@trocafone.com <manuel.aquino@trocafone.com>
2020-04-04 00:38:54 -0300

    sort code

--- public_html/createIrrigationPlan.php
+++ public_html/createIrrigationPlan.php
@@ -1,5 +1,5 @@
 <?php
-// http://localhost:9999/createIrrigationPlan.php?name='nuevo'&irrigation_plan_id=1&humidity_min_allowed=10&light_max_allowed=20&temperature_max_allowed=40&enabled=1
+// http://localhost:9999/createIrrigationPlan.php?name='nuevo'&humidity_min_allowed=10&light_max_allowed=20&temperature_max_allowed=40&enabled=1
 include('connectDb.php');
 
 $name = isset($_GET["name"]) ? $_GET["name"] : '';
@@ -7,72 +7,42 @@ $humidity_min_allowed = isset($_GET["humidity_min_allowed"]) ? $_GET["humidity_m
 $light_max_allowed = isset($_GET["light_max_allowed"]) ? $_GET["light_max_allowed"] : '';
 $temperature_max_allowed = isset($_GET["temperature_max_allowed"]) ? $_GET["temperature_max_allowed"] : '';
 $enabled = isset($_GET["enabled"]) ? $_GET["enabled"] : 1;
-	
 
+//INPUTS VALIDATIONS
+if ($light_max_allowed > 1) {
+	header('HTTP/1.0 400 Error');
+	echo 'La luminosidad debe ser 0 o 1';
+	exit;
+}
 
-//yo agregue todo hasta $query y el if que le sigue
+$select_query = "SELECT name FROM irrigations_plan WHERE name = $name";
+$select_query_result = $conexion->query($select_query) or die('error');
 
-$consulta = "SELECT * from irrigations_plan where name = $name";
-$resultado = $conexion->query($consulta) or die('error');
-//print_r($resultado);
 $results = [];
-//print_r($results);
 
-while($row = $resultado->fetch_assoc()) {
+while($row = $select_query_result->fetch_assoc()) {
     $results[] = $row;
 	//print_r($row);
 }
 
-
-if ((count($results) == 1)) {
-	header('HTTP/1.0 401 Unauthorized');
+if (count($results) == 1) {
+	header('HTTP/1.0 400 Error');
 	echo 'El plan de riego que intenta cargar ya existe, debe volver al listado y editarlo.';
 	exit;
-	}
-
-else {
-	if ($light_max_allowed > 1) {
-	header('HTTP/1.0 400 Error');
-	echo 'La luminosidad debe ser 0 o 1';
-	exit;
-	}
-	else {
-		$query = "INSERT INTO `irrigations_plan` (`name`, `humidity_min_allowed`, `light_max_allowed`, `temperature_max_allowed`, `enabled`) 
-		VALUES ({$name}, {$humidity_min_allowed}, {$light_max_allowed}, {$temperature_max_allowed}, {$enabled})";
-//echo "por aca paso";
-//print_r($query);
-		$resultado1 = $conexion->query($query) or die('error');
-//print_r($resultado1);
-		$results1 = [];
-		}
-//print_r($results1);
-
-/*while($row1 = $resultado1->fetch_assoc()) {
-	echo $row["name"] . ', ' . $row["humidity_min_allowed"] . ', ' . $row["light_max_allowed"] . ', ' . $row["temperature_max_allowed"]. ', ' . $row["enabled"];
-    $results1[] = $row1;
-	print_r($row1);
-}*/
-
-// echo $query;
-//print_r($query);
-//echo "El plan se cargo";
 }
 
 
+//CREATE IRRIGATION PLAN
+$insert_query = "INSERT INTO `irrigations_plan` (`name`, `humidity_min_allowed`, `light_max_allowed`, `temperature_max_allowed`, `enabled`) VALUES ({$name}, {$humidity_min_allowed}, {$light_max_allowed}, {$temperature_max_allowed}, {$enabled})";
+//echo($insert_query);
 
-
-
-//echo json_encode($results);
-
-if (!$resultado1 = $conexion->query($consulta)) {
+if (!$conexion->query($insert_query)) {
     echo "Error:\n";
-    echo $consulta;
-    die();
+    echo $query;
+    exit;
 }
-
 
 $conexion ->close();
-
 echo ("OK");
 
 ?>
